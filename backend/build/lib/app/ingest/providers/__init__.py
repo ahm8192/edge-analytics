7f8b98d6927b0c35c22dfc31caee99aa04f12922
ahmet <ahm8192@gmail.com>
2026-09-02 00:59:49 +0300

    chore: yerel build/ artefaktlarini kaldir, gitignore'a ekle

--- backend/build/lib/app/ingest/providers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Provider, ProviderCapabilities, RawRecord, RateLimiter
-from .registry import ProviderRegistry, orchestrate
-
-__all__ = ["Provider", "ProviderCapabilities", "RawRecord", "RateLimiter",
-           "ProviderRegistry", "orchestrate"]
